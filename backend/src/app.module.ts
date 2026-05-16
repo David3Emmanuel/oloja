c754a47e4e1972512006d7d9279e7a9ea5f146dd
David3Emmanuel <david3emmanuel@gmail.com>
2026-05-16 02:24:38 +0100

    (backend) Import new modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { HttpModule } from '@nestjs/axios'
+import { SquadModule } from './squad/squad.module'
+import { UsersModule } from './users/users.module'
+import { OpportunitiesModule } from './opportunities/opportunities.module'
+import { TrustModule } from './trust/trust.module'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    HttpModule,
+    SquadModule,
+    UsersModule,
+    OpportunitiesModule,
+    TrustModule,
+  ],
 })
 export class AppModule {}
